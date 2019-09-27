142b592fd3352775bd6d7d5f9f137985ef8747cf
Joe <joedravarol@gmail.com>
2019-09-27 22:53:10 +0100

    Add submit form's data

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
 let myLibrary = [];
-
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-};
-
-function addBookToLibrary() {
-
-};
+const bookColors = ["yellow", "teal", "pink", "beige", "light-blue", "grey", "light-beige", "light-grey"]
 
 const eventHandlers = {
   form: {
@@ -17,20 +7,26 @@ const eventHandlers = {
     modalForm: document.querySelector('.form-overlay'),
     saveFormBtn: document.getElementById('save'),
     cancelFormBtn: document.getElementById('cancel')
-  }  
+  }
 }
 
-// Events Listener
-// Form Modal Button
-eventHandlers.form.modalBtn.addEventListener('click', () => {
-  // Display form
-  toggleDisplayProperty(eventHandlers.form.modalBtn, eventHandlers.form.modalForm)
-});
+function Book(title, author, pages, read = false) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+};
 
-eventHandlers.form.cancelFormBtn.addEventListener('click', () => {
-  // Hide Form
-  toggleDisplayProperty(eventHandlers.form.modalBtn, eventHandlers.form.modalForm)
-});
+function addBookToLibrary(title, author, pages) {
+  let book = new Book(title, author, pages)
+
+  myLibrary.push(book);
+};
+
+const toggleFormModal = () => {
+  clearInputFields();
+  toggleDisplayProperty(eventHandlers.form.modalBtn, eventHandlers.form.modalForm);
+};
 
 // Toggle display value from block to none and vice versa
 const toggleDisplayProperty = (...events) => {
@@ -45,5 +41,35 @@ const toggleDisplayProperty = (...events) => {
       event.style.display = "block";
     }
   });
-
 };
+
+const clearInputFields = () => {
+  const inputs = document.querySelectorAll('input');
+
+  inputs.forEach((input) => {
+    input.value = "";
+  });
+};
+
+// Events Listener
+// Form Modal Button
+eventHandlers.form.modalBtn.addEventListener('click', () => {
+  // Display form
+  toggleFormModal();
+});
+
+eventHandlers.form.cancelFormBtn.addEventListener('click', () => {
+  // Hide Form
+  toggleFormModal();
+});
+
+// Submit Form
+document.querySelector('form').addEventListener('submit', (e) => {
+  e.preventDefault()
+  const title = e.target.title.value;
+  const author = e.target.author.value;
+  const pages = e.target.pages.value;
+
+  addBookToLibrary(title, author, pages);
+  toggleFormModal();
+});
