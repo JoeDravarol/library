69cf6f27eb3cecf1a5c8efdb4a4e7185d6546393
Joe <joedravarol@gmail.com>
2019-10-01 00:38:55 +0100

    Setup Firebase & Firestore database

---
+++ init-firebase.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyCllhzW21LMXCjQP5_SrYP4rUk5qcHojX4",
+  authDomain: "fir-library-8ef5c.firebaseapp.com",
+  databaseURL: "https://fir-library-8ef5c.firebaseio.com",
+  projectId: "fir-library-8ef5c",
+  storageBucket: "fir-library-8ef5c.appspot.com",
+  messagingSenderId: "113060513125",
+  appId: "1:113060513125:web:bd500a27e7115f179c4d8c"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
